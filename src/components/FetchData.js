77d0b4c3201db48f60801cf5ee30085b29dba9ee
Sulfikkar PR <sulfikkar.pr@phases.io>
2023-10-20 14:15:53 +0530

    Data folder created

--- src/components/FetchData.js
+++ src/components/FetchData.js
@@ -5,14 +5,29 @@ function FetchData() {
     const [post, setPost] = useState({})
 
     useEffect(() => {
-        axios
-            .get('https://narutodb.xyz/api/character')
-            .then(res => {
-                console.log(res.data)
-                setPost(res.data)
-            })
-            .catch(err => { console.log('error') })
-    })
+        // axios
+        //     .get('https://narutodb.xyz/api/character', {
+        //         mode: 'no-cors',
+        //         headers: {
+        //             "Content-Type": "application/json",
+        //             'Access-Control-Allow-Origin': '*',
+        //             'Access-Control-Allow-Methods': '*',
+        //             'Access-Control-Allow-Headers': '*',
+        //             'Access-Control-Allow-Credentials': true
+        //         },
+
+        //     })
+        //     .then(res => {
+        //         console.log(res.data)
+        //         setPost(res.data)
+        //     })
+        //     .catch(err => { console.log('error') })
+
+
+
+
+
+    }, []);
 
     return (
         <div>FetchData</div>
